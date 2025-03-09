b59fe225f775dcbd3d3c98a7f30e55d8c00eaebd
khs189 <khs189@naver.com>
2025-03-09 23:51:20 +0900

    d
    d

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/av2/", // GitHub Pages용 base 경로 설정
+})
